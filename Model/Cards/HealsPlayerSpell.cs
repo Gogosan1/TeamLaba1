b462bb9dbc9789111a0589671c3abb901b891112
Kate.S <ksalikhova03@icloud.com>
2023-01-11 12:21:24 +0700

    Divided cards classes

---
+++ Model/Cards/HealsPlayerSpell.cs
@@ -0,0 +1,25 @@
+﻿using Modlel.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LabaTeam1.Model.Cards
+{
+    public class HealsPlayerSpell : ICard
+    {
+        public string Name { get; init; }
+        [JsonIgnore]
+        public int Health { get; set; }
+        public int Power { get; set; }
+        [JsonConstructor]
+        public HealsPlayerSpell(string Name, int Power)
+        {
+            this.Name = Name;
+            this.Power = Power;
+            Health = 0;
+        }
+    }
+}
